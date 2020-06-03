08f7c99e466658959380d01d5f361bda32fc806e
Kushagra Karira <kushagrakarira@gmail.com>
2020-06-04 01:12:11 +0530

    Added

--- Hacking/applebanana996/README.md
+++
@@ -1,8 +0,0 @@
-# Windows-10-Activator
-Forked from https://github.com/applebanana996/Windows-10-Activator/
-by user https://github.com/applebanana996 ( applebanana996 )
-
-Modified by Kushagra Karira
-
-Instructions : Run as administrator
-
--- Hacking/applebanana996/Windows 10 Activator.bat
+++ Hacking/applebanana996/Windows 10 Activator.bat
@@ -1,5 +1,16 @@
 @echo off
-title Windows 10 ALL version activator&cls&echo ************************************&echo Supported products:&echo - Windows 10 Home&echo - Windows 10 Professional&echo - Windows 10 Enterprise, Enterprise LTSB&echo - Windows 10 Education&echo.&echo.&echo ************************************ &echo Windows 10 activation...
+title Windows 10 activator&
+cls&
+echo ************************************&
+echo Supported products:&
+echo - Windows 10 Home&
+echo - Windows 10 Professional&
+echo - Windows 10 Enterprise, Enterprise LTSB&
+echo - Windows 10 Education&
+echo.&
+echo.&
+echo ************************************ &
+echo Windows 10 activation...
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul
 cscript //nologo c:\windows\system32\slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul
@@ -21,5 +32,5 @@ if %i%==4 set KMS_Sev=AlwaysSmile.uk.to
 if %i%==5 set KMS_Sev=kms.chinancce.com 
 if %i%==6 exit
 cscript //nologo c:\windows\system32\slmgr.vbs /skms %KMS_Sev% >nul
-cscript //nologo c:\windows\system32\slmgr.vbs /ato | find /i "successfully" && (echo.& echo ************************************ & echo. & choice /n /c YN /m "Do you want to restart your PC now [Y,N]?" & if errorlevel 2 exit) || (echo The connection to the server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
+cscript //nologo c:\windows\system32\slmgr.vbs /ato | find /i "successfully" && (echo.& echo ************************************ & echo. & choice /n /c YN /m "Restart now [Y,N]?" & if errorlevel 2 exit) || (echo The connection to the server failed! Trying to connect to another one... & echo Please wait... & echo. & echo. & set /a i+=1 & goto server)
 shutdown.exe /r /t 00
